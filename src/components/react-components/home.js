9b33f76da7f71eca740c12e27890548973c6595a
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-12-21 17:24:01 -0600

    added navbar and carousel locations

--- src/components/react-components/home.js
+++ src/components/react-components/home.js
@@ -3,9 +3,17 @@ import "./home.css"
 function Home() {
   return (
     <div className="home">
-      <div className="logo-wrapper">
+      <header className="logo-wrapper">
         <div className="logo">
           <h1>LOGO</h1>
+        </div>
+      </header>
+      <nav className="navbar">
+        <h1>This is the navbar</h1>
+      </nav>
+      <div className="carousel-wrapper">
+        <div className="carousel">
+          <h1>This is where the carousel goes</h1>
         </div>
       </div>
     </div>

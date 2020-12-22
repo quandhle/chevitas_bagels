7f31404d80065f9d3b2b6f1f8f13667e86b3f168
PaulKillian <psk65lava@gmail.com>
2020-12-22 14:26:09 -0800

    added javascript and css media queries

--- src/components/menu.js
+++ src/components/menu.js
@@ -2,34 +2,67 @@ import React from 'react'
 import './menu.css'
 
 export default function Menu() {
-	return (
-		<>
-			<nav className="navbar">
-				<h1>This is the navbar</h1>
-			</nav>
-			<div className="social-media-wrapper">
-				<div className="social-media">
-					<span class="material-icons">facebook</span>
-					<span class="material-icons">facebook</span>
-					<span class="material-icons">facebook</span>
+	const x = window.matchMedia('(min-width: 376px)');
+	if (x.matches) {
+		return (
+			<>
+				<nav className="navbar">
+					<h1>This is the navbar</h1>
+				</nav>
+				<div className="social-media-wrapper">
+					<div className="social-media">
+						<span class="material-icons">facebook</span>
+						<span class="material-icons">facebook</span>
+						<span class="material-icons">facebook</span>
+					</div>
 				</div>
-			</div>
-			<main className="menu">
-				<div className="menu-items-left">
-					<h2>SANDWICHES</h2>
-					<h2>SALADS</h2>
-					<h2>BAGELS</h2>
+				<main className="menu">
+					<div>
+						<h2>SANDWICHES</h2>
+						<h2>SALADS</h2>
+						<h2>BAGELS</h2>
+					</div>
+					<div className="vertical-divider"></div>
+					<div>
+						<h2>SMOOTHIES</h2>
+						<h2>AGUA FRESCA</h2>
+						<h2>SOUPS</h2>
+						<h2>JUICES</h2>
+						<h2>DRINKS</h2>
+						<h2>DESSERTS</h2>
+					</div>
+				</main>
+			</>
+		)
+	} else {
+			return (
+			<>
+				<nav className="navbar">
+					<h1>This is the navbar</h1>
+				</nav>
+				<div className="social-media-wrapper">
+					<div className="social-media">
+						<span class="material-icons">facebook</span>
+						<span class="material-icons">facebook</span>
+						<span class="material-icons">facebook</span>
+					</div>
 				</div>
-				<div className="vertical-divider"></div>
-				<div className="menu-items-right">
-					<h2>SMOOTHIES</h2>
-					<h2>AGUA FRESCA</h2>
-					<h2>SOUPS</h2>
-					<h2>JUICES</h2>
-					<h2>DRINKS</h2>
-					<h2>DESSERTS</h2>
-				</div>
-			</main>
-		</>
-	);
+				<main className="menu">
+					<div className="menu-items">
+						<h2>SANDWICHES</h2>
+						<h2>SALADS</h2>
+						<h2>BAGELS</h2>
+					</div>
+					<div className="menu-items">
+						<h2>SMOOTHIES</h2>
+						<h2>AGUA FRESCA</h2>
+						<h2>SOUPS</h2>
+						<h2>JUICES</h2>
+						<h2>DRINKS</h2>
+						<h2>DESSERTS</h2>
+					</div>
+				</main>
+			</>
+		);
+	}
 }

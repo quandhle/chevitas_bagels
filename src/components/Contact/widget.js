560cfe3a3dfdba161c81f44e626d5d246fb9efee
MichaelPooleLF <michael.johnpoole@gmail.com>
2020-12-29 17:14:45 -0600

    modified semantic html a bit

--- src/components/Contact/widget.js
+++ src/components/Contact/widget.js
@@ -22,12 +22,12 @@ export default function ContactWidget(props) {
           <p>8am - 3pm</p>
         </div>
       </div>
-      <section className="map-frame">
-        <section className="map">
+      <div className="map-frame">
+        <div className="map">
           <iframe title="chevita's juice and bagels location" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3317.4834773503026!2d-117.87124618479288!3d33.748170280689784!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80dcd911d3f49e81%3A0x95a1f369d22a8b3c!2sChevita&#39;s%20Juice%20%26%20Bagels!5e0!3m2!1sen!2sus!4v1609192593088!5m2!1sen!2sus">
           </iframe>
-        </section>
-      </section>
+        </div>
+      </div>
     </section>
   )
 }

0acd46fc90403e533198e5399dd27af5ed1841e7
PaulKillian <psk65lava@gmail.com>
2020-12-28 16:03:11 -0800

    photo gallery update

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,33 +1,28 @@
 import "./home.css"
 import Navbar from '../Navbar'
+import Carousel from '../Carousel'
 
 function Home({setView}) {
   return (
-    <div className="home">
-      <div className="carousel-wrapper">
-        <div className="carousel">
-          <span className="material-icons">arrow_back_ios</span>
-          <h1>This is where the carousel goes</h1>
-          <span className="material-icons">arrow_forward_ios</span>
-        </div>
-      </div>
-      <div className="social-media-wrapper">
-        <div className="social-media">
-          <span className="material-icons">facebook</span>
-          <span className="material-icons">facebook</span>
-          <span className="material-icons">facebook</span>
-          <span className="material-icons">facebook</span>
-          <span className="material-icons">facebook</span>
-          <span className="material-icons">facebook</span>
-        </div>
-      </div>
-      <div className="contact-wrapper">
-        <div className="contact">
-          <h1>This is the contact section</h1>
-        </div>
-      </div>
-    </div>
-  )
+		<div className="home">
+			<Carousel />
+			<div className="social-media-wrapper">
+				<div className="social-media">
+					<span className="material-icons">facebook</span>
+					<span className="material-icons">facebook</span>
+					<span className="material-icons">facebook</span>
+					<span className="material-icons">facebook</span>
+					<span className="material-icons">facebook</span>
+					<span className="material-icons">facebook</span>
+				</div>
+			</div>
+			<div className="contact-wrapper">
+				<div className="contact">
+					<h1>This is the contact section</h1>
+				</div>
+			</div>
+		</div>
+  );
 }
 
 export default Home;
---
+++ src/components/Photos/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import './photos.css'
+
+export default function Photos() {
+	return (
+		<div className="container">
+			<div className="flex-grid">
+				<div className="photo">
+					<img className="image" className="image" src="./images/bagel-juice.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/rancheo-bagel.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/bagel-juice.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/san1.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/san2.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/san3.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/red-smoothie.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/green-smoothie.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/orange-drink.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/bagel-juice.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/bagel-juice.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/bagel-juice.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/bagel-juice.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/bagel-juice.jpg"></img>
+				</div>
+				<div className="photo">
+					<img className="image" src="./images/bagel-juice.jpg"></img>
+				</div>
+			</div>
+		</div>
+	);
+}

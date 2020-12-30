1dbdb7c9916ee8ea7f3e933f4d8e3d1c52c6bcf5
PaulKillian <psk65lava@gmail.com>
2020-12-30 13:21:38 -0800

    changed grid

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -46,84 +46,83 @@ class Carousel extends React.Component {
 	}
 
 	render() {
-			console.log(this.state.hrefString)
-			return (
-				<>
-					<div className="row flex justify-content-center carousel-wrapper text-white ">
-						<section className="carousel mt-5" aria-label="Gallery">
-							<div className="carousel__viewport align">
-								<li id="carousel__slide1" className="carousel__slide">
-									<div>
-										<img className="image1"src="/images/san1.jpg" alt=""></img>
-										<div className="carousel__snapper"></div>
-									</div>
+		return (
+			<>
+				<div className="row flex justify-content-center carousel-wrapper text-white ">
+					<section className="carousel mt-5" aria-label="Gallery">
+						<div className="carousel__viewport align">
+							<li id="carousel__slide1" className="carousel__slide">
+								<div>
+									<img className="image1"src="/images/san1.jpg" alt=""></img>
 									<div className="carousel__snapper"></div>
-								</li>
-								<li id="carousel__slide2" className="carousel__slide">
-									<div>
-										<img className="image1"src="/images/carousel1.jpg" alt="1"></img>
-										<div className="carousel__snapper"></div>
-									</div>
+								</div>
+								<div className="carousel__snapper"></div>
+							</li>
+							<li id="carousel__slide2" className="carousel__slide">
+								<div>
+									<img className="image1"src="/images/carousel1.jpg" alt="1"></img>
 									<div className="carousel__snapper"></div>
-								</li>
-								<li id="carousel__slide3" className="carousel__slide">
-									<div>
-										<img className="image1"src="/images/carousel2.jpg" alt="2"></img>
-										<div className="carousel__snapper"></div>
-									</div>
+								</div>
+								<div className="carousel__snapper"></div>
+							</li>
+							<li id="carousel__slide3" className="carousel__slide">
+								<div>
+									<img className="image1"src="/images/carousel2.jpg" alt="2"></img>
 									<div className="carousel__snapper"></div>
-								</li>
-								<li id="carousel__slide4" className="carousel__slide">
-									<div>
-										<img className="image1"src="/images/carousel3.jpg" alt="3"></img>
-										<div className="carousel__snapper"></div>
-									</div>
+								</div>
+								<div className="carousel__snapper"></div>
+							</li>
+							<li id="carousel__slide4" className="carousel__slide">
+								<div>
+									<img className="image1"src="/images/carousel3.jpg" alt="3"></img>
 									<div className="carousel__snapper"></div>
-								</li>
-								<li id="carousel__slide5" className="carousel__slide">
-									<div>
-										<img className="image1"src="/images/carousel4.jpg" alt="4"></img>
-										<div className="carousel__snapper"></div>
-									</div>
+								</div>
+								<div className="carousel__snapper"></div>
+							</li>
+							<li id="carousel__slide5" className="carousel__slide">
+								<div>
+									<img className="image1"src="/images/carousel4.jpg" alt="4"></img>
 									<div className="carousel__snapper"></div>
-								</li>
-								<li id="carousel__slide6" className="carousel__slide">
-									<div>
-										<img className="image1"src="/images/san2.jpg" alt="5"></img>
-										<div className="carousel__snapper"></div>
-									</div>
+								</div>
+								<div className="carousel__snapper"></div>
+							</li>
+							<li id="carousel__slide6" className="carousel__slide">
+								<div>
+									<img className="image1"src="/images/san2.jpg" alt="5"></img>
 									<div className="carousel__snapper"></div>
-								</li>
-								<li id="carousel__slide7" className="carousel__slide">
-									<div>
-										<img className="image1"src="/images/carousel6.jpg" alt="6"></img>
-										<div className="carousel__snapper"></div>
-									</div>
+								</div>
+								<div className="carousel__snapper"></div>
+							</li>
+							<li id="carousel__slide7" className="carousel__slide">
+								<div>
+									<img className="image1"src="/images/carousel6.jpg" alt="6"></img>
 									<div className="carousel__snapper"></div>
-								</li>
+								</div>
+								<div className="carousel__snapper"></div>
+							</li>
+						</div>
+						<aside className="carousel__navigation shadowed mb-5">
+							<div className="carousel__navigation-item">
+								<a onClick={this.updateCarouselHref} href={this.state.hrefString}>
+									<span className="material-icons" id="left">
+										arrow_back_ios
+									</span>
+								</a>
 							</div>
-							<aside className="carousel__navigation shadowed mb-5">
-								<div className="carousel__navigation-item">
-									<a onClick={this.updateCarouselHref} href={this.state.hrefString}>
-										<span className="material-icons" id="left">
-											arrow_back_ios
-										</span>
-									</a>
-								</div>
-								<div className="carousel__navigation-item">
-									<a onClick={this.updateCarouselHref} href={this.state.hrefString}>
-										<span className="material-icons" id="right">
-											arrow_forward_ios
-										</span>
-									</a>
-								</div>
-							</aside>
-						</section>
-					</div>
-				</>
-			);
-		}
+							<div className="carousel__navigation-item">
+								<a onClick={this.updateCarouselHref} href={this.state.hrefString}>
+									<span className="material-icons" id="right">
+										arrow_forward_ios
+									</span>
+								</a>
+							</div>
+						</aside>
+					</section>
+				</div>
+			</>
+		);
 	}
+}
 
 
 export default Carousel;

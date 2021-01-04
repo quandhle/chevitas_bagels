dd2cdd4b3cbfac79cb11db06ba03f6f539a0fbd4
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-01-04 16:06:12 -0600

    got react router to work!

--- src/components/App.js
+++ src/components/App.js
@@ -11,37 +11,45 @@ import {Button} from '@material-ui/core';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 
 const App = () => {
-  const [view, setView] = useState("home")
   const [lang, setLang] = useState("en")
 
-  const viewComponent = lang => {
-    switch(view) {
-      case "menu":
-        return <Menu lang={lang}/>
-      case "ourStory":
-        return <OurStory lang={lang}/>
-      case 'photos':
-			  return <Photos lang={lang} />;
-      case "contactUs":
-        return <Contact lang={lang} load="page"/>
-      default:
-        return <Home lang={lang}/>;
-    }
-  }
-
   return (
-    <div className="App">
-      {view === "home" && <HeroImage type="logo"/>}
-      <Navbar lang={lang} setView={setView} />
-      <Button
-        onClick={() => lang === "en" ? setLang("sp") : setLang("en")}
-        color="primary"
-        variant="contained"
-      >
-          Click to change language.
-      </Button>
-      {viewComponent(lang)}
-    </div>
+    <Router>
+      <div className="App">
+        <HeroImage type="logo" />
+        <Navbar lang={lang}/>
+        <Button
+          onClick={() => lang === "en" ? setLang("sp") : setLang("en")}
+          color="primary"
+          variant="contained"
+        >
+            Click to change language.
+        </Button>
+        {/* {viewComponent(lang)} */}
+        <Switch>
+          <Route
+            path="/menu"
+            render={props => <Menu lang={lang} {...props} />}
+            />
+          <Route
+            path="/our-story"
+            render={props => <OurStory lang={lang} {...props}/>}
+            />
+          <Route
+            path="/photos"
+            render={props => <Photos lang={lang} {...props} />}
+            />
+          <Route
+            path="/contact-us"
+            render={props => <Contact lang={lang} load="page" {...props} />}
+            />
+          <Route
+            exact path="/"
+            render={props => <Home lang={lang} {...props} />}
+            />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,5 +1,6 @@
 import "./navbar.css";
 import {messages} from '../messages/messages';
+import {Link} from "react-router-dom";
 
 const Navbar = ({
   setView,
@@ -7,11 +8,26 @@ const Navbar = ({
 }) => (
   <nav className="navbar">
     <ul>
-      <li onClick={() => setView("home")}>{messages.navBarHome.message[lang]}</li>
+      {/* <li onClick={() => setView("home")}>{messages.navBarHome.message[lang]}</li>
       <li onClick={() => setView("menu")}>{messages.navBarMenu.message[lang]}</li>
       <li onClick={() => setView("ourStory")}>{messages.navBarOurStory.message[lang]}</li>
       <li onClick={() => setView("contactUs")}>{messages.navBarContactUs.message[lang]}</li>
-      <li onClick={() => setView("photos")}>{messages.navBarPhotos.message[lang]}</li>
+      <li onClick={() => setView("photos")}>{messages.navBarPhotos.message[lang]}</li> */}
+      <Link to="/">
+        <li>{messages.navBarHome.message[lang]}</li>
+      </Link>
+      <Link to="/menu">
+        <li>{messages.navBarMenu.message[lang]}</li>
+      </Link>
+      <Link to="/our-story">
+        <li>{messages.navBarOurStory.message[lang]}</li>
+      </Link>
+      <Link to="/contact-us">
+        <li>{messages.navBarContactUs.message[lang]}</li>
+      </Link>
+      <Link to="/photos">
+        <li>{messages.navBarPhotos.message[lang]}</li>
+      </Link>
     </ul>
   </nav>
 )

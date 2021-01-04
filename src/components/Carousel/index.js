c3ca029e42658df0a316a238ab5297f00716dd78
PaulKillian <psk65lava@gmail.com>
2021-01-04 10:57:27 -0800

    further updates to make carousel responsive

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -6,12 +6,12 @@ import 'react-alice-carousel/lib/alice-carousel.css';
 const handleDragStart = e => e.preventDefault();
 
 const items = [
-	<img src="/images/bagel-juice.jpg" autoWidth onDragStart={handleDragStart} className="image1" />,
-	<img src="/images/ranchero-bagel.jpg" onDragStart={handleDragStart} className="image1" />,
-	<img src="/images/san1.jpg" onDragStart={handleDragStart} className="image1" />,
-	<img src="/images/carousel3.jpg" onDragStart={handleDragStart} className="image1" />,
-	<img src="/images/san2.jpg" onDragStart={handleDragStart} className="image1" />,
-	<img src="/images/carousel1.jpg" onDragStart={handleDragStart} className="image1" />
+	<img src="/images/bagel-juice.jpg" autoWidth onDragStart={handleDragStart} className="image1" alt="bagel and juice" />,
+	<img src="/images/ranchero-bagel.jpg" onDragStart={handleDragStart} className="image1" alt="bagel"/>,
+	<img src="/images/san1.jpg" onDragStart={handleDragStart} className="image1" alt="sandwich" />,
+	<img src="/images/carousel3.jpg" onDragStart={handleDragStart} className="image1" alt="sandwich" />,
+	<img src="/images/san2.jpg" onDragStart={handleDragStart} className="image1" alt="sandwich" />,
+	<img src="/images/carousel1.jpg" onDragStart={handleDragStart} className="image1" alt="" />
 ];
 
 const Carousel = () => {

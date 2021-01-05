04a792a237a112d53752c922c09b0f997b1c8eaa
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-01-05 13:59:32 -0600

    gave contact page a background

--- src/components/Hero-Image/index.js
+++ src/components/Hero-Image/index.js
@@ -2,7 +2,7 @@ import "./hero-image.css"
 
 export default function HeroImage({heroStyles, imageText}) {
   return (
-    <header className="hero-image-wrapper" style={!heroStyles.header ? null: heroStyles.header}>
+    <header className="hero-image-wrapper" style={heroStyles.header}>
       <div className="hero-image" style={heroStyles.image}>
         <h1 style={heroStyles.text}>{imageText}</h1>
       </div>

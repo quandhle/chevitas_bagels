91f326a3577bbc127f5f9379ca4c1768e08e87dc
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-01-06 11:33:49 -0600

    added relavent links to menu social media icons

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -30,9 +30,15 @@ export default function Menu() {
 				<div className="menu-wrapper">
 					<div className="social-media-wrapper">
 						<div className="social-media">
-							<img src={grubhub} alt="grubhub icon"></img>
-							<img src={doordash} alt="doordash icon"></img>
-							<img src={call} alt="call icon"></img>
+							<a href="https://www.grubhub.com/restaurant/chevitas-juice-and-bagel-209-w-4th-st-santa-ana/1472683" target="_blank" rel="noreferrer">
+								<img src={grubhub} alt="grubhub icon"></img>
+								</a>
+							<a href="https://www.doordash.com/store/chevita-s-juice---bagels-santa-ana-783486/en-US" target="_blank" rel="noreferrer">
+								<img src={doordash} alt="doordash icon"></img>
+								</a>
+							<a href="tel:7148523650" target="_blank" rel="noreferrer">
+								<img src={call} alt="call icon"></img>
+								</a>
 						</div>
 					</div>
 					<main className="menu">

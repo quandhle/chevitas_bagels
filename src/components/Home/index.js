3bccba00a64d9175c0841a1548de1fc7c0b27fa3
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-01-06 11:21:05 -0600

    icons on home have relevant links now

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -27,12 +27,24 @@ function Home() {
       <Carousel />
       <div className="social-media-wrapper-home">
         <div className="social-media-home">
-          <img src={doordash} alt="doordash icon"></img>
-          <img src={instagram} alt="instagram icon"></img>
-          <img src={yelp} alt="yelp icon"></img>
-          <img src={facebook} alt="facebook icon"></img>
-          <img src={call} alt="call icon"></img>
-          <img src={grubhub} alt="grubhub icon"></img>
+          <a href="https://www.doordash.com/store/chevita-s-juice---bagels-santa-ana-783486/en-US" target="_blank" rel="noreferrer">
+            <img src={doordash} alt="doordash icon"></img>
+            </a>
+          <a href="https://www.instagram.com/chevitasdtsa/" target="_blank" rel="noreferrer">
+            <img src={instagram} alt="instagram icon"></img>
+            </a>
+          <a href="https://www.yelp.com/biz/chevitas-juice-and-bagel-santa-ana" target="_blank" rel="noreferrer">
+            <img src={yelp} alt="yelp icon"></img>
+            </a>
+          <a href="https://www.facebook.com/ChevitasJB" target="_blank" rel="noreferrer">
+            <img src={facebook} alt="facebook icon"></img>
+            </a>
+          <a href="tel:7148523650" target="_blank" rel="noreferrer">
+            <img src={call} alt="call icon"></img>
+            </a>
+          <a href="https://www.grubhub.com/restaurant/chevitas-juice-and-bagel-209-w-4th-st-santa-ana/1472683" target="_blank" rel="noreferrer">
+            <img src={grubhub} alt="grubhub icon"></img>
+            </a>
         </div>
       </div>
       <ContactUs load="widget"/>

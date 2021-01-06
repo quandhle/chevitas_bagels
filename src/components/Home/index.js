a1cf55dd79cff77ebc5cbd07b15c47934b1a6a74
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-01-05 18:40:39 -0600

    created and added grubhub icon

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,6 +7,7 @@ import instagramIcon from "../images/instagram-icon.svg";
 import yelpIcon from "../images/yelp-icon.svg";
 import facebookIcon from "../images/facebook-icon.svg";
 import callIcon from "../images/call-icon.svg";
+import grubhubIcon from "../images/grubhub-icon.svg";
 
 function Home() {
   const heroStyles = {
@@ -30,11 +31,11 @@ function Home() {
       <div className="social-media-wrapper-home">
         <div className="social-media-home">
           <span className="material-icons">facebook</span>
-          <span className="material-icons">facebook</span>
           <img src={instagramIcon} alt="instagram icon"></img>
           <img src={yelpIcon} alt="yelp icon"></img>
           <img src={facebookIcon} alt="facebook icon"></img>
           <img src={callIcon} alt="phone call icon"></img>
+          <img src={grubhubIcon} alt="grubhub icon"></img>
         </div>
       </div>
       <ContactUs load="widget"/>

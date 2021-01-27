7d05d703819f38aad9128560f85403339c2666ec
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-01-27 11:38:39 -0600

    added spanish translations for home page

---
+++ src/components/messages/home.js
@@ -0,0 +1,28 @@
+export const homeItems = {
+  contactWidget: {
+    header: {
+      english: [
+        "Contact",
+        "Hours",
+        "Location"
+      ],
+      spanish: [
+        "Contacto",
+        "Horario",
+        "Dirección"
+      ]
+    },
+    body: {
+      english: [
+        "Contact",
+        "Hours",
+        "Monday - Sunday"
+      ],
+      spanish: [
+        "Contacto",
+        "Horario",
+        "Lunes - Domingo"
+      ]
+    }
+  }
+}

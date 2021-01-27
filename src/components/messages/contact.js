5fb82c930bf562e5a440b8757bec3058f7b5bd6b
MichaelPooleLF <michael.johnpoole@gmail.com>
2021-01-27 12:36:58 -0600

    added change language to contact page

---
+++ src/components/messages/contact.js
@@ -0,0 +1,16 @@
+export const contactText = {
+  page: {
+    en: [
+      "Phone Number",
+      "Hours of Operation",
+      "Monday - Sunday",
+      "Come Visit Us!"
+    ],
+    sp: [
+      "Teléfono",
+      "Horario",
+      "Lunes - Domingo",
+      "¡Venga a Visitarnos!"
+    ]
+  }
+}

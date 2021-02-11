36e147cb1546e8666ee22caf9a4cefea41ca876d
PaulKillian <psk65lava@gmail.com>
2021-02-10 16:56:25 -0800

    changed main log height because of class changes on live site

--- src/components/Hero-Image/index.js
+++ src/components/Hero-Image/index.js
@@ -17,7 +17,7 @@ const styles = {
     backgroundSize: "contain",
     backgroundRepeat: "no-repeat",
     overflow: "hidden",
-    minHeight: "300px",
+    minHeight: "69vh",
     minWidth: "300px",
     maxHeight: "500px",
     maxWidth: "500px",

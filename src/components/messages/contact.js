9b110f8353bd7db44a9b4a19a2c82457bc6c3f12
PaulKillian <psk65lava@gmail.com>
2021-02-17 09:59:30 -0800

    fixed merge on messages/contact.js

--- src/components/messages/contact.js
+++ src/components/messages/contact.js
@@ -1,47 +1,3 @@
-<<<<<<< HEAD
-export const contactText = {
-  page: {
-    en: [
-      "Phone Number",
-      "Hours of Operation",
-      "Monday - Sunday",
-      "Come Visit Us!"
-    ],
-    sp: [
-      "Teléfono",
-      "Horario",
-      "Lunes - Domingo",
-      "¡Venga a Visitarnos!"
-    ]
-  },
-  contactWidget: {
-    en: {
-      header: [
-        "Contact",
-        "Hours",
-        "Location"
-      ],
-      body: [
-        "Contact",
-        "Hours",
-        "Monday - Sunday"
-      ]
-    },
-    sp: {
-      header: [
-        "Contacto",
-        "Horario",
-        "Dirección"
-      ],
-      body: [
-        "Contacto",
-        "Horario",
-        "Lunes - Domingo"
-      ]
-    }
-  }
-}
-=======
 export const contactText = {
   page: {
     en: [
@@ -88,4 +44,3 @@ export const contactText = {
     }
   }
 }
->>>>>>> upstream/main
